3b6028b7a9c1fcd02e39028e29e6ee6f7cf84043
Nikita Shabulin <nwpress@yandex.ru>
2020-09-30 09:26:55 +0700

    Added product cart

--- src/js/modules/App.js
+++ src/js/modules/App.js
@@ -8,13 +8,15 @@ var webstore = new Vue({
   el: '#app',
   data: {
     sitename: 'Vue.js Pet Depot',
+    showProduct: true,
     product: {
       id: 1001,
       title: 'Cat Food, 25lb bag',
       description: 'A 25 pound bag of <em>irresistible</em>,' +
         'organic goodness for your cat.',
       price: 2000,
-      image: 'src/assets/images/product-fullsize.png'
+      image: 'src/assets/images/product-fullsize.png',
+      availableInventory: 5
     },
     cart: []
   },
@@ -40,11 +42,17 @@ var webstore = new Vue({
   methods: {
     addToCart: function () {
       this.cart.push(this.product.id);
+    },
+    showCheckout() {
+      this.showProduct = this.showProduct ? false : true;
     }
   },
   computed: {
     cartItemCount: function () {
       return this.cart.length || '';
+    },
+    canAddToCart: function() {
+      return this.product.availableInventory > this.cartItemCount;
     }
   }
 });
